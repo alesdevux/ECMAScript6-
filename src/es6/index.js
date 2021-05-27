9290b7c129197850dadacb31104a5a7e8e1e50c1
aleswebgit <aleswebgit@gmail.com>
2021-05-27 18:16:07 +0200

    clases

--- src/es6/index.js
+++ src/es6/index.js
@@ -151,3 +151,26 @@ helloPromise()
   .then(response => console.log(response))
   .then(() => console.log('hola'))
   .catch(error => console.log(error));
+
+
+// CLASES 
+class calculator {
+  constructor() {
+    this.valueA = 0;
+    this.valueB = 0;
+  }
+  sum(valueA, valueB) {
+    this.valueA = valueA;
+    this.valueB = valueB;
+    return this.valueA + this.valueB;
+  }
+  rest(valueA, valueB) {
+    this.valueA = valueA;
+    this.valueB = valueB;
+    return this.valueA - this.valueB;
+  }
+}
+
+const calc = new calculator();
+console.log(calc.sum(2,2));
+console.log(calc.rest(7,5));
