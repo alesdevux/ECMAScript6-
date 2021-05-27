3b8cbde5a1f301a5ee2843031850fc3f36aa1282
aleswebgit <aleswebgit@gmail.com>
2021-05-27 18:01:25 +0200

    promesas

--- src/es6/index.js
+++ src/es6/index.js
@@ -134,3 +134,20 @@ const listOfNamesConstOneES6 = name => {
 }
 
 const square = num => num * num;
+
+
+// PROMESAS
+const helloPromise = () => {
+  return new Promise((resolve, reject) => {
+    if (false) {
+      resolve('Hey!');
+    } else {
+      reject('Ups!!');
+    }
+  });
+}
+
+helloPromise()
+  .then(response => console.log(response))
+  .then(() => console.log('hola'))
+  .catch(error => console.log(error));
