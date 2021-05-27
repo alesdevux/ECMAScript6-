4c6aa17dd3493b024a3ab31911602e657142a99b
aleswebgit <aleswebgit@gmail.com>
2021-05-27 13:59:16 +0200

    template literals

--- src/es6/index.js
+++ src/es6/index.js
@@ -13,3 +13,16 @@ function datosDefectoES6(name = 'Ales', age = 25, country = 'CA') {
 
 datosDefectoES6();
 datosDefectoES6('Antonio', 34, 'MX');
+
+
+// TEMPLATE LITERALS
+let hello = 'Hello';
+let world = 'World';
+
+// Antes 
+let tipicalFrase = hello + ' ' + world;
+console.log(tipicalFrase);
+
+// Con ES6
+let tipicalFrase2 = `${hello} ${world}`;
+console.log(tipicalFrase2);
