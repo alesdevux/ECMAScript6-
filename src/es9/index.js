62e8fe6a5e13d15341da4bec475e5aae6d103ab4
aleswebgit <aleswebgit@gmail.com>
2021-05-30 18:03:05 +0200

    es9: regex, trabajar con fechas

--- src/es9/index.js
+++ src/es9/index.js
@@ -33,3 +33,13 @@ helloWorld()
   .then(response => console.log(response))
   .catch(error => console.log(error))
   .finally(() => console.log('Finalizo'));
+
+
+// Regex -> trabajar con fechas
+const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/;
+const match = regexData.exec('2021-04-23');
+const year = match[1];
+const month = match[2];
+const day = match[3];
+
+console.log(year, month, day);
