e723bcf1cbb50b59855e8801c6ba410ae7713912
aleswebgit <aleswebgit@gmail.com>
2021-05-30 16:58:12 +0200

    async - await

--- src/es8/index.js
+++ src/es8/index.js
@@ -20,3 +20,28 @@ console.log(values.length);
 const string = 'hello';
 console.log(string.padStart(7, 'hi'));
 console.log(string.padEnd(12, ' ------'));
+
+
+// Async - Await
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    (true) ? setTimeout(() => resolve('Hello World'), 3000) : reject(new Error('Test Error'));
+  });
+}
+
+const helloAsync = async () => {
+  const hello = await helloWorld();
+  console.log(hello);
+}
+helloAsync();
+
+// ejecutar correctamente
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld();
+    console.log(hello);
+  } catch (error) {
+    console.log(error);
+  }
+}
+anotherFunction();
