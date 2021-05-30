a3ad38e1f0d8ed269174444d02d7825db90e97c3
aleswebgit <aleswebgit@gmail.com>
2021-05-30 13:36:21 +0200

    es7: includes y asignación de exponenciación

---
+++ src/es7/index.js
@@ -0,0 +1,14 @@
+let numbers = [1, 2, 3, 7, 8];
+
+if (numbers.includes(9)) {
+  console.log('Si que inluye el número 7');
+} else {
+  console.log('No inluye el número 7');
+}
+
+
+let base = 4;
+let exponent = 4;
+let result = base ** exponent;
+
+console.log(result);
