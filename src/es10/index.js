51332ef7a993b8ccdf3a21d5374adee1ca24eb8a
aleswebgit <aleswebgit@gmail.com>
2021-05-30 18:22:16 +0200

    es10: trimStart y trimEnd

--- src/es10/index.js
+++ src/es10/index.js
@@ -6,3 +6,13 @@ console.log(array.flat(2));
 let array = [1,2,3,4,5];
 console.log(array.flatMap(value => [value, value * 2]));
 
+
+// trimStart
+let hello = '            hello world';
+console.log(hello);
+console.log(hello.trimStart());
+
+// trimEnd
+let hello = 'hello world            ';
+console.log(hello);
+console.log(hello.trimEnd());
