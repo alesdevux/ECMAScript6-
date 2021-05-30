06736d0c36d6da2107de81a0f92f032303e0fb53
aleswebgit <aleswebgit@gmail.com>
2021-05-30 17:58:42 +0200

    es9: .finally() en las promesas

--- src/es9/index.js
+++ src/es9/index.js
@@ -18,3 +18,18 @@ const obj1 = {
   twitter: 'alesweb'
 };
 console.log(obj1);
+
+
+// Promise Finally
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    (true)
+      ? setTimeout(() => resolve('Hello World'), 3000)
+      : reject(new Error('Test Error'))
+  });
+}
+
+helloWorld()
+  .then(response => console.log(response))
+  .catch(error => console.log(error))
+  .finally(() => console.log('Finalizo'));
